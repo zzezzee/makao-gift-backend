d2f03d3bcac719de11f8d1b845cbee055b989ab5
zzezzee <GitHub zzezzee@naver.com>
2022-11-30 18:34:03 +0900

    project setting

---
+++ src/main/java/com/zzezze/makaogift/MakaogiftApplication.java
@@ -0,0 +1,31 @@
+package com.zzezze.makaogift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class MakaogiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakaogiftApplication.class, args);
+	}
+
+	@Bean
+	public WebSecurityCustomizer ignoringCustomizer() {
+		return (web) -> web.ignoring().requestMatchers("/**");
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
+}
---
+++ src/test/java/com/zzezze/makaogift/MakaogiftApplicationTests.java
@@ -0,0 +1,15 @@
+package com.zzezze.makaogift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class MakaogiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
